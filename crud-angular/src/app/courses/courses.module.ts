e44e67fe2cab05ead3ae05706a4211d3326a4f78
Caio Bomfim <caaiobomfim@gmail.com>
2024-05-26 15:25:25 -0300

    import module toolbar and card

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,17 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class CoursesModule { }
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { Course } from '../models/course';
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
   imports: [
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
@@ -15,7 +20,7 @@ import { Course } from '../models/course';
 export class CoursesComponent {
 
   courses: Course[] = [
-    {_id: '1', name: 'Angular', category: 'front-end'}
+    {_id: '1', name: 'Angular', category: 'Front-end'}
   ];
   displayedColumns = ['name','category'];
 
